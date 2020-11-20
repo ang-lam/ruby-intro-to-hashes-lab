8d304edd45cd6217b569aae8b9d99560080ec70f
Angela <angelalamluu@gmail.com>
2020-11-19 18:11:28 -0800

    commit tree a833c3a811cf78598318aa845913f6872f721383

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,27 +1,50 @@
 def new_hash
-
+	n_hash = {}
 end
 
 def actor
-
+	actor_hash = {name: "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {}
-
+	mon = {railroads: {} }
+	mon
 end
 
 def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
+	mon = monopoly
+	#can set new variable to mon[:railroads] so don't have to type it for each line
+	monRail = mon[:railroads]
+	monRail[:pieces] = 4
+	monRail[:names] = {}
+	monRail[:rent_in_dollars] = {}
+	mon
 end
 
 def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
+	third_tier = monopoly_with_second_tier
+	rent = third_tier[:railroads][:rent_in_dollars]
+	rent[:one_piece_owned] = 25
+	rent[:two_pieces_owned] = 50
+	rent[:three_pieces_owned] = 100
+	rent[:four_pieces_owned] = 200
+	names = third_tier[:railroads][:names]
+	names[:reading_railroad] = {}
+	names[:pennsylvania_railroad] = {}
+	names[:b_and_o_railroad] = {}
+	names[:shortline] = {}
+	third_tier
 end
 
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-
+	fourth_tier = monopoly_with_third_tier
+	names = fourth_tier[:railroads][:names]
+	names[:reading_railroad]["mortgage_value"] = "$100"
+	names[:pennsylvania_railroad]["mortgage_value"] = "$200"
+	names[:b_and_o_railroad]["mortgage_value"] = "$400"
+	names[:shortline]["mortgage_value"] = "$800"
+	fourth_tier
 end
